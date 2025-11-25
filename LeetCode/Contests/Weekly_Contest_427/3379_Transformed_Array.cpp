8185bbba6ade70e31081694f467405bffecb43f2
AshishPrasad07 <ashishprasad462@gmail.com>
2025-11-25 21:00:19 +0530

    Added Time & Space Complexity Comment

--- LeetCode/Contests/Weekly_Contest_427/3379_Transformed_Array.cpp
+++ LeetCode/Contests/Weekly_Contest_427/3379_Transformed_Array.cpp
@@ -8,24 +8,27 @@ For each index i, move nums[i] steps right if positive, left if negative,
 or leave it if zero, and set the result array with the value at landing index.
 */
 
+// Time Complexity:  O(N)
+// Space Complexity: O(N)
+// N = Length of Array
 class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n, 0); // Result array initialized with 0s
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0){
                 // Move nums[i] steps to the right in circular manner
                 int index = (i + nums[i]) % n;
                 ans[i] = nums[index];
             } 
-            else if (nums[i] < 0) {
+            else if(nums[i] < 0){
                 // Move |nums[i]| steps to the left in circular manner
                 int index = (nums[i] % n + n + i) % n;
                 ans[i] = nums[index];
             } 
-            else {
+            else{
                 // nums[i] == 0, leave as is
                 ans[i] = nums[i];
             }
